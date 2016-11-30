8cef751d10a4a09abb3e13a13288445c616d3f8a
Stewart Hughes <stewvhughes@gmail.com>
2016-11-30 16:59:33 -0500

    Color Sensor's Input into TeleOp

--- ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcLionsTeleOp.java
+++ ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcLionsTeleOp.java
@@ -21,8 +21,52 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
-@TeleOp(name="TeleOp", group="TeleOp")
+@TeleOp(name="TeleOp", group="TeleOp")  //TELEOP!
+
 public class FTCLionsTeleOp extends OpMode {
+
+    private Camera camera;
+    public CameraPreview preview;
+    public Bitmap image;
+    private int width;
+    private int height;
+    private YuvImage yuvImage = null;
+    private int looped = 0;
+    private String data;
+
+    private int red(int pixel) {
+        return (pixel >> 16) & 0xff;
+    }
+
+    private int green(int pixel) {
+        return (pixel >> 8) & 0xff;
+    }
+
+    private int blue(int pixel) {
+        return pixel & 0xff;
+    }
+
+    private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+        public void onPreviewFrame(byte[] data, Camera camera)
+        {
+            Camera.Parameters parameters = camera.getParameters();
+            width = parameters.getPreviewSize().width;
+            height = parameters.getPreviewSize().height;
+            yuvImage = new YuvImage(data, ImageFormat.NV21, width, height, null);
+            looped += 1;
+        }
+    };
+
+    private void convertImage() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, width, height), 0, out);
+        byte[] imageBytes = out.toByteArray();
+        image = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+    /*
+     * Code to run when the op mode is first enabled goes here
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+     */
 
     final boolean DEBUG = true;
 
@@ -52,9 +96,25 @@ public class FTCLionsTeleOp extends OpMode {
 
     @Override
     public void init() {
+        camera = ((FtcRobotControllerActivity)hardwareMap.appContext).camera;
+        camera.setPreviewCallback(previewCallback);
 
+        Camera.Parameters parameters = camera.getParameters();
+        data = parameters.flatten();
+
+        ((FtcRobotControllerActivity) hardwareMap.appContext).initPreview(camera, this, previewCallback);
     }
 
+    public int highestColor(int red, int green, int blue) {
+        int[] color = {red,green,blue};
+        int value = 0;
+        for (int i = 1; i < 3; i++) {
+            if (color[value] < color[i]) {
+                value = i;
+            }
+        }
+        return value;
+    }
 
     @Override
     public void loop() {
@@ -64,6 +124,36 @@ public class FTCLionsTeleOp extends OpMode {
 //            telemetry.addData("Gamepad1 Movement: y", gamepad1.right_stick_y, ", x", gamepad1.right_stick_x);
 //            telemetry.addData("Gamepad2 Movement: y", gamepad2.right_stick_y, ", x", gamepad2.right_stick_x);
         }
+
+        if (yuvImage != null) {  //color sensor output and value swaps
+            int redValue = 0;
+            int blueValue = 0;
+            int greenValue = 0;
+            convertImage();
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    int pixel = image.getPixel(x, y);
+                    redValue += red(pixel);
+                    blueValue += blue(pixel);
+                    greenValue += green(pixel);
+                }
+            }
+            int color = highestColor(redValue, greenValue, blueValue);
+            String colorString = "";
+            switch (color) {
+                case 0:
+                    colorString = "RED";
+                    break;
+                case 1:
+                    colorString = "GREEN";
+                    break;
+                case 2:
+                    colorString = "BLUE";
+            }
+            telemetry.addData("Color:", "Color detected is: " + colorString);
+        }
+        telemetry.addData("Looped","Looped " + Integer.toString(looped) + " times");
+        Log.d("DEBUG:",data);
 
         ////////////////////////////////
         //     GAMEPAD 1 CONTROLS     //
